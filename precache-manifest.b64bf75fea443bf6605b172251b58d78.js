2cc75db1f3b43bd33350b07dbad5350bd9f69e80
HHJ-S2 <berdle0122@gmail.com>
2020-10-04 14:36:43 +0900

    Updates

---
+++ precache-manifest.b64bf75fea443bf6605b172251b58d78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "639633debe5449350d1e1d6864d69ca5",
+    "url": "/habit_tracker/index.html"
+  },
+  {
+    "revision": "a3bbae12fd1031b52986",
+    "url": "/habit_tracker/static/css/main.717e3633.chunk.css"
+  },
+  {
+    "revision": "bac6724d66ed20f9d320",
+    "url": "/habit_tracker/static/js/2.79b2f4ed.chunk.js"
+  },
+  {
+    "revision": "746582258d70325461391e08406982a5",
+    "url": "/habit_tracker/static/js/2.79b2f4ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3bbae12fd1031b52986",
+    "url": "/habit_tracker/static/js/main.654ebadf.chunk.js"
+  },
+  {
+    "revision": "b9f33d5c3a47a69f892b",
+    "url": "/habit_tracker/static/js/runtime-main.39175f41.js"
+  }
+]);
